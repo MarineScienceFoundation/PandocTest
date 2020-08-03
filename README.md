c2a1ba5c0cc023bb3425909fa9df1a7d9e02e1da
pookiemaker <pookiemaker@gmail.com>
2020-08-03 11:44:00 -0700

    more changes
    more changes

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 
 # Marine Science Foundation
+## Abstract
 
-## double has
+Quick Brown Fox
 
+# Chapter Alpha
 
-# PDF Manual
-[manual.pdf](https://github.com/MarineScienceFoundation/PandocTest/releases/download/nightly-build/manual.pdf)
+Slow Rabbit
+
+## Frog
+
+Fast Frog
+
+# Chapter Bravo
+
+Clone Wars
--- makebook.sh
+++ makebook.sh
@@ -1,16 +1,13 @@
-pandoc -o MSF_template.pdf \
+pandoc -o  MSF_template.pdf \
   title.txt \
   README.md \
+  --include-before-body cover.tex \
   --variable mainfont="DejaVu Sans" \
   --variable monofont="DejaVu Sans Mono" \
   --variable fontsize=12pt \
-  --variable geometry:"top=1.5cm, bottom=2.5cm, left=1.5cm, right=1.5cm" \
   --variable geometry:a4paper \
   --variable number-sections \
-  --variable documentclass=report \
   --table-of-contents \
-  --toc-depth=2 \
+  --toc-depth=1 \
   --top-level-division=chapter \
-  --reference-links \
   --pdf-engine=pdflatex \
-  --number-sections
