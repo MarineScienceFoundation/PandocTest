c2fa5eb23a5ca67af7020f5650120fbfc0209680
pookiemaker <pookiemaker@gmail.com>
2020-08-07 16:08:13 -0700

    trimming and approaching v1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Pandoc example.
+---------
+
+This template shows a simple example of building a documentation book from
+assorted readme & markdown files in your source tree.
+
+For a mac ---
+
+# Requirements
+* *[MacTex](https://tug.org/mactex/)*
+
+* *popplar*
+```ruby -e
+   "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    < /dev/null 2> /dev/null
+    ```
--- examples/build-book.sh
+++ examples/build-book.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cp "../eisvogel.tex" "../eisvogel.latex"
+cp "../templates/eisvogel.tex" "../templates/eisvogel.latex"
 
 echo "##"
 echo "# Building book example"
@@ -8,38 +8,10 @@ echo "##"
 echo ""
 
 
-#echo "- book"
-# Additional options like `book: true` and `classoption: [oneside]` are set in the markdown file via the YAML metadata block.
-#pandoc "book/book.md" -o "book/book.pdf" --from markdown --template "../eisvogel.latex" --listings --top-level-division="chapter"
-#pdftoppm -r 150 -png "book/book.pdf" > "book/book.png"
-
-
-#echo "- titlepage-background"
-#cd "titlepage-background"
-#pandoc "titlepage-background.md" -o "titlepage-background.pdf" --from markdown --template "../../eisvogel.latex" --listings
-#pdftoppm -r 150 -png "titlepage-background.pdf" > "titlepage-background.png"
-#cd ".."
-
-#echo "- custom-titlepage"
-#cd "custom-titlepage"
-#pandoc "custom-titlepage.md" -o "custom-titlepage.pdf" --from markdown --template "../../eisvogel.latex" --listings
-#pdftoppm -r 150 -png "custom-titlepage.pdf" > "custom-titlepage.png"
-#cd ".."
-
-
-
-#echo "- MSF logo-titlepage"
-# Important: Since the logo is a manually included image, the option "--resource-path" can't be used for specifying its location.
-# The location of the logo has to be relative to where pandoc is executed, so we cd into the directory with the logo.
-#cd "MSF-titlepage"
-#pandoc --top-level-division=chapter "logo-titlepage.md" -o "logo-titlepage.pdf" --from markdown --template "../../eisvogel.latex" --listings
-#pdftoppm -r 150 -png "logo-titlepage.pdf" > "logo-titlepage.png"
-#cd ".."
-
 echo "- custom-titlepage"
 cd "../custom-titlepage"
-pandoc --top-level-division=chapter "custom-titlepage.md" -o "custom-titlepage.pdf" --from markdown --template "../eisvogel.latex" --listings
-pdftoppm -r 150 -png "custom-titlepage.pdf" > "custom-titlepage.png"
+pandoc --top-level-division=chapter "custom-titlepage.md" -o "../output/custom-titlepage.pdf" --from markdown --template "../templates/eisvogel.latex" --listings
+pdftoppm -r 150 -png "../output/custom-titlepage.pdf" > "../output/custom-titlepage.png"
 cd ".."
 
 
